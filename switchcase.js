1bc58a4b84b0ad9e526ddbff3195debd47df9e07
fairuzulum <usleunmdi32@gmail.com>
2023-07-19 01:23:58 +0700

    switch case

---
+++ switchcase.js
@@ -0,0 +1,16 @@
+let language = 'English';
+let greeting = null;
+
+switch(language){
+    case "Indonesia":
+        greeting = 'Selamat Pagi';
+        break;
+    case "English":
+        greeting = 'Good Morning';
+        break;
+    case "Japanese":
+        greeting = 'Ohayou Gozaimasu';
+        break;
+}
+
+console.log(greeting);
