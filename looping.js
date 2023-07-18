1481ab6c09294833b7e25766034456a9f56aab2d
fairuzulum <usleunmdi32@gmail.com>
2023-07-19 01:42:11 +0700

    loop

---
+++ looping.js
@@ -0,0 +1,25 @@
+// For Loop
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+
+// For of Loop
+let color = ['red','green','blue'];
+for(const allcolor of color){
+    console.log(allcolor);
+}
+
+
+// While and Do-While
+let x = 1;
+while(x <= 10){
+    console.log(x);
+    x++;
+}
+
+let y = 1;
+do{
+    console.log(y);
+    y++;
+}while(y <= 10);
