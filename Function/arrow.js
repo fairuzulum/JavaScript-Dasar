d350b4f9469e2f8ec5ab70503a8799ebfb18470a
fairuzulum <usleunmdi32@gmail.com>
2023-07-24 14:10:59 -0700

    Arrow Function

---
+++ Function/arrow.js
@@ -0,0 +1,13 @@
+// Arrow Function
+const sayName = (name) => {
+    console.log(`Nama saya ${name}`)
+}
+sayName('Fairuz');
+
+//
+const sayHello = (name) => console.log(`Hallo ${name}`);
+sayHello("Fairuz");
+
+//
+const multiply = (a,b) => a*b;
+console.log(multiply(2,3));
